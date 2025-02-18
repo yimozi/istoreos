c33814341d90b05eea74e3a3c5106a29d887b195
yimozi <92222881+yimozi@users.noreply.github.com>
2025-02-18 09:47:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,14 @@
 # echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 # echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
 # echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+# 更新最新OpenClash源码
+# 更新openclash内核
+git clone --single-branch --depth 1 -b dev https://github.com/vernesong/OpenClash.git package/zhyoi/luci-app-openclash
+wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz --no-check-certificate
+mkdir -p files/etc/openclash/core/
+tar -zxvf clash-linux-amd64.tar.gz  -C files/etc/openclash/core/
+mv files/etc/openclash/core/clash files/etc/openclash/core/clash_meta
+rm -rf clash-linux-amd64.tar.gz
+curl -sL --connect-timeout 5 -m 30 --speed-time 15 --speed-limit 1 --retry 2 https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat -o files/etc/openclash/geoip.dat
+curl -sL --connect-timeout 5 -m 30 --speed-time 15 --speed-limit 1 --retry 2 https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat -o files/etc/openclash/GeoSite.dat
+curl -sL --connect-timeout 5 -m 30 --speed-time 15 --speed-limit 1 --retry 2  https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country.mmdb -o files/etc/openclash/Country.mmdb
